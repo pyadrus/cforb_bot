d26b7e81062b714d65323af32f803ce8cb9312d5
pyadminrus <zh.vitaliy92@yandex.ru>
2023-12-08 22:52:20 +0300

    Добавлен функционал к пункту меню "Партнерские условия для посредников"

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from loguru import logger
 
 from handlers.greeting_handlers import register_greeting_handler
 from handlers.order_form_handlers import register_order_form_handler
+from handlers.partnership_conditions_for_intermediaries_handlers import \
+    register_partnership_conditions_for_intermediaries_handler
 from handlers.services_and_prices_handlers import register_services_and_prices_handler
 from handlers.types_of_packaging_handlers import register_types_of_packaging_handler
 from system.dispatcher import dp
@@ -20,6 +22,7 @@ def main() -> None:
     register_services_and_prices_handler()
     register_order_form_handler()  # Кнопка "Бланк заказа"
     register_types_of_packaging_handler()  # Кнопка "Виды упаковки"
+    register_partnership_conditions_for_intermediaries_handler() # Партнерские условия для посредников
 
 
 if __name__ == '__main__':
