9fc85acd43de2f27ff982e88715f318a9a7e54e0
PyAdminRU <zh.vitaliy92@gmail.com>
2025-05-23 12:10:26 +0300

    Рефакторинг кода

--- system/dispatcher.py
+++ system/dispatcher.py
@@ -1,15 +1,16 @@
-import configparser
+#import configparser
 
 from aiogram import Bot, Dispatcher, Router
 from aiogram.fsm.storage.memory import MemoryStorage
 
-config = configparser.ConfigParser(empty_lines_in_values=False, allow_no_value=True)
-config.read("setting/config.ini")  # Чтение файла
-BOT_TOKEN = config["BOT_TOKEN"]["BOT_TOKEN"]
+#config = configparser.ConfigParser(empty_lines_in_values=False, allow_no_value=True)
+#config.read("setting/config.ini")  # Чтение файла
+#BOT_TOKEN = config["BOT_TOKEN"]["BOT_TOKEN"]
+
+BOT_TOKEN = "6979734406:AAE_xRbLTVu8Crbus2L5Px0QIXlvdjABYnc"
 
 bot = Bot(
     token=BOT_TOKEN,
-    # parse_mode="HTML"
 )
 storage = MemoryStorage()  # Хранилище
 dp = Dispatcher(storage=storage)
